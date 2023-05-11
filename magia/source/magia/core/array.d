363f2c409063dfc2bbb4ca6945e687506b720ee5
Enalye <enalyen@gmail.com>
2023-05-11 23:50:17 +0200

    Màj classe Array

--- magia/source/magia/core/array.d
+++ magia/source/magia/core/array.d
@@ -1,58 +1,66 @@
-/**
-    IndexedArray
-
-    Copyright: (c) Enalye 2017
-    License: Zlib
-    Authors: Enalye
-*/
-
-module magia.core.indexedarray;
+/** 
+ * Copyright: Enalye
+ * License: Zlib
+ * Authors: Enalye
+ */
+module magia.core.array;
 
 import std.parallelism;
 import std.range;
 import std.typecons;
 
-/**
-	Special Array that remove fragmentation while keeping indexes valid.
-*/
-class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
-    private uint _dataTop = 0u;
-    private uint _availableIndexesTop = 0u;
-    private uint _removeTop = 0u;
+/// Liste supprimant la fragmentation tout en gardant les index valides.
+final class Array(T, size_t _capacity, bool _useParallelism = false) {
+    private size_t _dataTop = 0u;
+    private size_t _availableIndexesTop = 0u;
+    private size_t _removeTop = 0u;
 
     private T[_capacity] _dataTable;
-    private uint[_capacity] _availableIndexes;
-    private uint[_capacity] _translationTable;
-    private uint[_capacity] _reverseTranslationTable;
-    private uint[_capacity] _removeTable;
+    private size_t[_capacity] _availableIndexes;
+    private size_t[_capacity] _translationTable;
+    private size_t[_capacity] _reverseTranslationTable;
+    private size_t[_capacity] _removeTable;
 
     @property {
-        uint length() const {
+        /// Nombre d’éléments contenus
+        size_t length() const {
             return _dataTop;
         }
 
-        uint capacity() const {
+        /// Capacité maximale que peut contenir la liste
+        size_t capacity() const {
             return _capacity;
         }
 
+        /// Liste des éléments contenus
         ref T[_capacity] data() {
             return _dataTable;
         }
+
+        /// La liste est-elle vide ?
+        bool empty() const {
+            return _dataTop == 0;
+        }
+
+        /// La liste est-elle pleine ?
+        bool full() const {
+            return (_dataTop + 1u) == _capacity;
+        }
     }
 
-    uint push(T value) {
-        uint index;
+    /// Ajoute un élément à la liste
+    size_t push(T value) {
+        size_t index;
 
         if ((_dataTop + 1u) == _capacity) {
-            throw new Exception("IndexedArray overload");
+            throw new Exception("array overload");
         }
 
         if (_availableIndexesTop) {
             //Take out the last available index on the list.
             _availableIndexesTop--;
             index = _availableIndexes[_availableIndexesTop];
-        }
-        else {
+        } else {
             //Or use a new id.
             index = _dataTop;
         }
@@ -67,8 +75,9 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
         return index;
     }
 
-    void pop(uint index) {
-        uint valueIndex = _translationTable[index];
+    /// Retire un élément de la liste
+    void pop(size_t index) {
+        size_t valueIndex = _translationTable[index];
 
         //Push the index on the available indexes stack.
         _availableIndexes[_availableIndexesTop] = index;
@@ -80,39 +89,36 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
         //Take the top value on the stack and fill the gap.
         _dataTop--;
         if (valueIndex < _dataTop) {
-            uint userIndex = _reverseTranslationTable[_dataTop];
+            size_t userIndex = _reverseTranslationTable[_dataTop];
             _dataTable[valueIndex] = _dataTable[_dataTop];
             _translationTable[userIndex] = valueIndex;
             _reverseTranslationTable[valueIndex] = userIndex;
         }
     }
 
+    /// Vide la liste
     void reset() {
         _dataTop = 0u;
         _availableIndexesTop = 0u;
         _removeTop = 0u;
     }
 
-    void markInternalForRemoval(uint index) {
-        synchronized {
-            _removeTable[_removeTop] = _reverseTranslationTable[index];
-            _removeTop++;
-        }
-    }
-
-    void markForRemoval(uint index) {
+    /// Marque un élément à supprimer
+    void mark(size_t index) {
         _removeTable[_removeTop] = index;
         _removeTop++;
     }
 
-    void sweepMarkedData() {
-        for (uint i = 0u; i < _removeTop; i++) {
+    /// Supprime tous les éléments marqué pour suppression
+    void sweep() {
+        for (size_t i = 0u; i < _removeTop; i++) {
             pop(_removeTable[i]);
         }
         _removeTop = 0u;
     }
 
     static if (_useParallelism) {
+        /// Itère sur la liste
         int opApply(int delegate(ref T) dlg) {
             int result;
 
@@ -125,8 +131,8 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
 
             return result;
         }
-    }
-    else {
+    } else {
+        /// Ditto
         int opApply(int delegate(ref T) dlg) {
             int result;
 
@@ -141,6 +147,7 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
         }
     }
 
+    /// Ditto
     int opApply(int delegate(const ref T) dlg) const {
         int result;
 
@@ -155,11 +162,12 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
     }
 
     static if (_useParallelism) {
-        int opApply(int delegate(ref T, uint) dlg) {
+        /// Ditto
+        int opApply(int delegate(const size_t, ref T) dlg) {
             int result;
 
             foreach (i; parallel(iota(_dataTop))) {
-                result = dlg(_dataTable[i], i);
+                result = dlg(_reverseTranslationTable[i], _dataTable[i]);
 
                 if (result)
                     break;
@@ -167,13 +175,13 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
 
             return result;
         }
-    }
-    else {
-        int opApply(int delegate(ref T, uint) dlg) {
+    } else {
+        /// Ditto
+        int opApply(int delegate(const size_t, ref T) dlg) {
             int result;
 
             foreach (i; 0u .. _dataTop) {
-                result = dlg(_dataTable[i], i);
+                result = dlg(_reverseTranslationTable[i], _dataTable[i]);
 
                 if (result)
                     break;
@@ -183,11 +191,12 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
         }
     }
 
-    int opApply(int delegate(const ref T, uint) dlg) const {
+    /// Ditto
+    int opApply(int delegate(const size_t, const ref T) dlg) const {
         int result;
 
         foreach (i; 0u .. _dataTop) {
-            result = dlg(_dataTable[i], i);
+            result = dlg(_reverseTranslationTable[i], _dataTable[i]);
 
             if (result)
                 break;
@@ -196,11 +205,12 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
         return result;
     }
 
-    int opApply(int delegate(const Tuple!(const uint, const T)) dlg) const {
+    /// Ditto
+    int opApply(int delegate(const Tuple!(const size_t, const T)) dlg) const {
         int result;
 
         foreach (i; 0u .. _dataTop) {
-            result = dlg(tuple!(const uint, const T)(_reverseTranslationTable[i], _dataTable[i]));
+            result = dlg(tuple!(const size_t, const T)(_reverseTranslationTable[i], _dataTable[i]));
 
             if (result)
                 break;
@@ -209,11 +219,18 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
         return result;
     }
 
-    T opIndex(uint index) {
+    /// Accède à un élément
+    T opIndex(size_t index) {
         return _dataTable[_translationTable[index]];
     }
 
-    bool has(uint index) {
+    /// Ditto
+    T opIndexAssign(T value, size_t index) {
+        return _dataTable[_translationTable[index]] = value;
+    }
+
+    /// L’index est-il valide ?
+    bool has(size_t index) {
         if (index > _dataTop)
             return false;
         if (_translationTable[index] == -1)
@@ -221,14 +238,14 @@ class IndexedArray(T, uint _capacity, bool _useParallelism = false) {
         return true;
     }
 
-    /// Returns the first element in the list
-    T first() {
+    /// Returne le premier élément dans la liste
+    T front() {
         assert(_dataTop > 0);
         return _dataTable[0];
     }
 
-    /// Returns the last element in the list
-    T last() {
+    /// Returne le dernier élément dans la liste
+    T back() {
         assert(_dataTop > 0);
         return _dataTable[_dataTop - 1];
     }
--- magia/source/magia/core/package.d
+++ magia/source/magia/core/package.d
@@ -9,20 +9,21 @@
 module magia.core;
 
 public {
-    import magia.core.resource;
+    import magia.core.array;
     import magia.core.color;
-    import magia.core.indexedarray;
     import magia.core.json;
-    import magia.core.quat;
     import magia.core.mat;
+    import magia.core.quat;
+    import magia.core.resource;
     import magia.core.ringbuffer;
     import magia.core.simplex;
+    import magia.core.singleton;
     import magia.core.spline;
     import magia.core.stream;
-    import magia.core.singleton;
     import magia.core.timer;
     import magia.core.timestep;
     import magia.core.transform;
+    import magia.core.tuple;
     import magia.core.util;
     import magia.core.vec;
 }
